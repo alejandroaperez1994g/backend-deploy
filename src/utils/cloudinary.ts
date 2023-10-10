85223579c47acd2ecbdbee2b06676429093b2456
Alejandro <alejandroaperez1994g@gmail.com>
2023-10-10 17:09:01 +0200

    refactor: update cloudinary

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -2,7 +2,11 @@ const cloudinary = require('cloudinary').v2
 
 import config from '../config/config'
 
-cloudinary.config(config.cloudinary)
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 export const uploadImage = async (imagePath: string) => {
     return await cloudinary.uploader.upload(imagePath,

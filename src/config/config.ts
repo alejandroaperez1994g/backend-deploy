8ce4baf09fbf1f57aaad36f2e8501d11533b0484
Alejandro <alejandroaperez1994g@gmail.com>
2023-10-10 20:23:45 +0200

    refactor: update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import Configuration from "../interfaces/config.interface";
 
 dotenv.config()
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV ?? 'development'
 
 const CONFIG: Configuration = {
     development: {
@@ -22,6 +22,22 @@ const CONFIG: Configuration = {
         }
 
     },
+    production: {
+        app: {
+            PORT: process.env.PORT || 4001
+        },
+        auth0: {
+            client_origin: process.env.APP_ORIGIN,
+            audience: process.env.AUTH0_AUDIENCE,
+            issuer: process.env.AUTH0_ISSUER
+        },
+        cloudinary: {
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        }
+
+    }
 
 }
 
